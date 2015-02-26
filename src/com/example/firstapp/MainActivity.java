15e638442a6d59463b76304d93561579c37382ee
onur00 <ontur00@gmail.com>
2015-02-26 20:31:15 +0100

    Initial Project Version

---
+++ src/com/example/firstapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.firstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	Button b;
+	CheckBox cb;
+	ImageView view;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		b = (Button) findViewById(R.id.button1);
+		cb = (CheckBox) findViewById(R.id.checkBox1);
+		view = (ImageView) findViewById(R.id.imageView1);
+		
+		b.setText("Probny tekst");		
+		String text = b.getText()+"";
+		
+		if( cb.isChecked() ){
+			b.setText("Zostalem zaznaczony");
+		}
+		else{
+			b.setText("Zosta³em odznaczony");
+		}
+		
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
